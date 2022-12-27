70b9670151252cece73776b3e29ec3af48fd7d03
apekshachachar <apekshachachar@gmail.com>
2022-12-27 16:11:48 +0530

    tagname_task

--- dom.js
+++ dom.js
@@ -31,13 +31,13 @@
 */
 
 //GET ELEMENTS BY CLASSNAME
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-items[2].style.backgroundColor='green'
-for(var i=0;i<items.length;i++)
-{
-    items[i].style.fontWeight='bold'
-}
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[2].style.backgroundColor='green'
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight='bold'
+// }
 
 
 //items[1].style.fontWeight='bold'
