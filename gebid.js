42593761fb57f14bcfad125a2533ed19792fea8b
apekshachachar <121419285+apekshachachar@users.noreply.github.com>
2022-12-29 15:56:00 +0530

    id and classname task

---
+++ gebid.js
@@ -0,0 +1,11 @@
+//GET ELEMENT BY ID
+
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px black'
+var title=document.getElementById('main')
+
+//GET ELEMENTS BY CLASS NAME
+var title=document.getElementsByClassName('title');
+console.log(title);
+title[0].style.fontWeight='bold'
+title[0].style.color='green'
