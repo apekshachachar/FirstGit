7613a76ec6c9d5349d3167f90864eba789c3ca7e
apekshachachar <121419285+apekshachachar@users.noreply.github.com>
2022-12-29 16:10:38 +0530

    getElementsByClassName

---
+++ cname.js
@@ -0,0 +1,9 @@
+//GET ELEMENTS BY CLASSNAME
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor='green'
+for(var i=0; i<items.length;i++)
+{
+    items[i].style.fontWeight='bold'
+}
